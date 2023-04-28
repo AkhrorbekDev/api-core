697a7bc66a5ad80e7824ebddaaa42ae629e9c881
axrorbek <axrorbeknis@icloud.com>
2023-04-28 16:08:22 +0500

    chore(release): v1.0.4

---
+++ src/runtime/types.ts
@@ -0,0 +1,39 @@
+import {$Fetch, FetchContext, FetchOptions} from "ofetch";
+
+interface ApiCoreInterface {
+  baseURL: string,
+  fetchOptions: $FetchOptions
+  _fetch: $Fetch
+
+  get(url: string, data: any, config: any): Promise<any>
+
+  post(url: string, data: any, config: any): Promise<any>
+
+  put(url: string, data: any, config: any): Promise<any>
+
+  delete(url: string, data: any, config: any): Promise<any>
+
+  head(url: string, data: any, config: any): Promise<any>
+
+  trace(url: string, data: any, config: any): Promise<any>
+
+  connect(url: string, data: any, config: any): Promise<any>
+
+  options(url: string, data: any, config: any): Promise<any>
+
+  config(args: ApiCoreInterface): this
+}
+
+type $FetchOptions = FetchOptions & {
+  options: { [index: string | number | symbol]: any } &
+    {
+      config: {
+        [index: string | number | symbol]: any
+      },
+      paramsSerializer: Function
+    }
+}
+
+type $FetchContext = Omit<FetchContext, 'options'> & { options: $FetchOptions['options'] }
+
+export {$FetchContext, $FetchOptions, ApiCoreInterface}
