96b401630699cc551825ae2b73600fb3deb3e2af
axrorbek <axrorbeknis@icloud.com>
2023-05-02 12:57:14 +0500

    fix

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,5 @@
 import { defineNuxtPlugin } from '#app'
-import ApiCore from './core/ApiCore'
+import { ApiCore } from './core'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.config.globalProperties.$apiCore = ApiCore
